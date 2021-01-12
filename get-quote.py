a593bbc4febce33fbaa4c1099fc33d8a4f487023
elaineqian02 <elaineqian@berkeley.edu>
2021-01-12 14:29:05 -0800

    print multiple quotes

--- get-quote.py
+++ get-quote.py
@@ -7,10 +7,10 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  for _ in range(3):
+    rnd = random.randint(0, last)
+    print(quotes[rnd], end='')
 
 if __name__== "__main__":
   primary()
